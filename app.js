14d22ccb2a5fcd608a5e3148dbc77a4b2fd19a69
samuelokellogum <oksam21@gmail.com>
2019-10-11 06:28:52 +0300

    user

--- app.js
+++ app.js
@@ -4,94 +4,68 @@ const chalk = require('chalk');
 const routes = require('./routes/index');
 const bodyParser = require('body-parser');
 const bcrypt = require('bcrypt');
-//const port = 8080;
 const app = express();
 
+app.use('/', routes);
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use('/routes', require('./routes/index'));
 app.use('/models', require('./models/user'));
 
-
-// view engine setup
+//View Engine Setup
 app.set('views', path.join(__dirname, 'views'));
 app.use(express.static(path.join(__dirname, 'public')));
 app.use(express.static(__dirname + '/views'));
+
+//Listening for the Port
+app.listen(port, () => console.log(`Listening on port" ${chalk.green(port)}`));
 
 //Load Index Page
 app.get('/', function(req, res) {
     res.sendFile(path.join(__dirname + '/index.html'));
 });
 
-//Sign-up
-app.post('/sign_up', function(req, res) {
-    var username = req.body.name;
-    var telephone = req.body.telephone;
-    var email = req.body.email;
-    var password = req.body.password;
+//Sign Up
+app.post('sign-up', function(req, res) {
+    user.findOne({ email: req.body.email }).then(user => {
+        if (user) {
+            //return res.status(res.statusCode >= 100 && res.statusCode < 600 ? err.code : 500).json({ email: "Email already exists" });
+            return res.status(400).json({ email: "Email already exists" });
+        } else {
+            const newUser = new User({
+                username: req.body.username,
+                telephone: req.body.telephone,
+                email: req.body.email,
+                password: req.body.password
+            });
 
-    var data = {
-        "username": username,
-        "telephone": telephone,
-        "email": email,
-        "password": password
-    }
-    db.collection('users').insertOne(data, function(err, collection) {
-        if (err) throw err;
-        console.log("Record inserted Successfully");
+            newUser.db("firstparking").collections("users").insertOne(req.body);
 
+            bcrypt.genSalt(10, (err, salt) => {
+                bcrypt.hash(newUser.password, salt, (err, hash) => {
+                    if (err) throw err;
+                    newUser.password = hash;
+                    newUser.save()
+                        .then(user => res.json(user))
+                        .catch(err => { console.log(err) })
+                });
+            });
+        }
     });
-
-    return res.redirect('profile.html');
+    //res.render('/profile.html');
+    res.redirect('/profile.html');
 });
 
-//Sign Up
-// app.post('sign-up', function(req, res) {
-//     user.findOne({ email: req.body.email }).then(user => {
-//         if (user) {
-//             //return res.status(res.statusCode >= 100 && res.statusCode < 600 ? err.code : 500).json({ email: "Email already exists" });
-//             return res.status(400).json({ email: "Email already exists" });
-//         } else {
-//             const newUser = new User({
-//                 username: req.body.username,
-//                 telephone: req.body.telephone,
-//                 email: req.body.email,
-//                 password: req.body.password
-//             });
-
-//             newUser.db("firstparking").collections("users").insertOne(req.body);
-
-//             bcrypt.genSalt(10, (err, salt) => {
-//                 bcrypt.hash(newUser.password, salt, (err, hash) => {
-//                     if (err) throw err;
-//                     newUser.password = hash;
-//                 S    newUser.save()
-//                         .then(user => res.json(user))
-//                         .catch(err => { console.log(err) })
-//                 });
-//             });
-//         }
-//     });
-//     res.render('/profile.html');
-//     //res.redirect('/profile.html');
-// });
-
-
-
-
-//route configuration
-app.use('/', routes);
-
-//listening for the port
-//app.listen(port, () => console.log(`Listening on port" ${chalk.green(port)}`));
-
-// catch 404 and forward to error handler
-app.use(function(err, req, res) {
+//Catch 404 and forward to error handler
+app.use(function(err, req, res, next) {
     //res.status(res.statusCode >= 100 && res.statusCode < 600 ? err.code : 500).send('Not Found Error');
+    if (err) {
+        next(err);
+    }
     res.status(404).send('Not Found Error');
 });
 
-//Development error handler. It will print stacktrace
+//Development Error Handler printing stacktrace
 if (app.get('env') === 'development') {
     app.use(function(err, req, res, next) {
         //res.status(res.statusCode >= 100 && res.statusCode < 600 ? err.code : 500);
@@ -100,9 +74,10 @@ if (app.get('env') === 'development') {
             message: err.message,
             error: err
         });
-
+        if (err) {
+            next(err);
+        }
     });
-    // render the error page
 }
 
 //Production error handler. No stacktraces
